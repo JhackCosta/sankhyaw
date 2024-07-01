9fde73af4a9854f5439000bd6154367c21bd4c78
Jhack Costa <jhack.costa@sankhya.com.br>
2024-07-01 14:38:26 -0300

    teste erro

---
+++ erros/src/programas/Programa.java
@@ -0,0 +1,15 @@
+package programas;
+
+public class Programa {
+	
+	public static void main(String[] args) throws Exception {
+		
+			
+		try {
+			System.out.println("Olá mundo");
+		} catch (Exception e) {
+			throw new Exception();
+		}
+	}
+
+}
